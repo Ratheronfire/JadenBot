357cc96e6437a38766655473fc72fbcd2b94adad
Ratheronfire <ratheronfire@gmail.com>
2015-02-24 12:16:52 -0500

    Added check for missing config/API key

--- jadenbot.py
+++ jadenbot.py
@@ -9,14 +9,45 @@ from wordnik import *
 from urllib2 import HTTPError
 from random import choice
 
+default_config = { "api-key": "", "parts-of-speech": 
+	[ "noun", "adjective", "verb", "adverb", "interjection", "pronoun",
+	"preposition", "abbreviation", "affix", "article", "auxiliary-verb",
+	"conjunction", "definite-article", "family-name", "given-name",
+	"idiom", "imperative", "noun-plural", "noun-posessive",
+	"past-participle", "phrasal-prefix", "proper-noun",
+	"proper-noun-plural", "proper-noun-posessive", "suffix",
+	"verb-intransitive", "verb-transitive" ],
+	"base-url": "http://api.wordnik.com/v4",
+	"min-length": 5, "max-length": 9,
+	"min-corpus-count": 5000, "max-corpus-count": -1,
+	"sentences":
+	[ "How Can [noun] Be [adjective] If Our [noun] Aren't [adjective] ?" ]
+}
+
+def prompt_for_api_key():
+	print "You must add your own API key from http://developer.wordnik.com" \
+	      "under the field 'api-key' to use this program."
+	exit(0)
+
+def initialize_config():
+	with open("config.json", "w") as config_file:
+		json.dump(default_config, config_file, indent=2)
+
 def load_config():
 	if os.access("config.json", os.F_OK):
 		with open("config.json", "r") as config_file:
 			config = json.load(config_file)
+	else:
+		print "No config file found.  Generating default file in config.json"
+		initialize_config()
+		prompt_for_api_key()
 	
 	return config
 
 def init_api():
+	if config["api-key"] == "":
+		prompt_for_api_key()
+	
 	return swagger.ApiClient(config["api-key"], config["base-url"])
 
 def get_word(partOfSpeech):
@@ -44,11 +75,13 @@ def generate_sentence():
 	words = choice(config["sentences"]).split(' ')
 	sentence = ""
 
-	for word in words:
-		word = re.sub("\[|\]", "", word)
-		if word in config["parts-of-speech"]:
-			word = get_word(word)
+	for source_word in words:
+		source_word = re.sub("\[|\]", "", source_word)
+		if source_word in config["parts-of-speech"]:
+			word = get_word(source_word)
 			word = word.capitalize()
+		else:
+			word = source_word
 		
 		if word in "?!.":
 			sentence = sentence.rstrip()
