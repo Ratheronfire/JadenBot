e575eef04cf86995b278d2149c70e9b3f84bb999
Ratheronfire <Ratheronfire@gmail.com>
2015-02-26 19:04:43 -0500

    Added additional IRC commands and general stability checks.

--- jaden_irc.py
+++ jaden_irc.py
@@ -6,19 +6,61 @@ from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
 from jadenbot import generate_sentence
 
 class JadenBot(irc.bot.SingleServerIRCBot):
-	config = dict()
+    config = dict()
 
-	def __init__(self, channel, nickname, server, config, port=6667):
-		irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
-		self.channel = channel
-		self.config = config
-	
-	def on_nicknameinuse(self, c, e):
-		c.nick(c.get_nickname() + "_")
+    def __init__(self, channel, nickname, server, config, port=6667):
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
+        self.channel = channel
+        self.config = config
 
-	def on_welcome(self, c, e):
-		c.join(self.channel)
-	
-	def on_pubmsg(self, c, e):
-		if e.arguments[0] == self.config["command-string"]:
-			c.notice(e.source.nick, generate_sentence())
+        print "Joining " + channel + " on " + server + ":" + str(port) + " as " + nickname
+
+    def print_sentence(self, c, e):
+        print "Read command " + self.config["command-string"] + " from " + e.source.nick
+
+        sentence = generate_sentence()
+            
+        print(sentence)
+        c.privmsg(self.channel, sentence)
+
+    def on_nicknameinuse(self, c, e):
+        c.nick(c.get_nickname() + "_")
+
+    def on_welcome(self, c, e):
+        c.join(self.channel)
+
+    def on_pubmsg(self, c, e):
+        msg = e.arguments[0]
+        user = e.source.nick
+
+        if msg == self.config["command-string"]:
+            self.print_sentence(c, e)
+        elif msg.lower() == "jadenbot die":
+            print "Read exit command from " + user
+            if user.lower() in self.config["bot-ops"]:
+                print "Exiting now."
+                self.die()
+            else:
+                print user.nick + " is not authorized to kill bot."
+                c.privmsg(self.channel, "JadenBot never dies")
+    
+    def on_privmsg(self, c, e):
+        msg = e.arguments[0]
+        msg_words = msg.split()
+        user = e.source.nick
+
+        if user.lower() in self.config["bot-ops"]:
+            print "Authorized user " + user + " issued private command " + msg
+            if msg_words[0] == "!say":
+                c.privmsg(self.channel, msg.replace("!say ", ""))
+            elif msg_words[0] == "!jaden":
+                self.print_sentence(c, e)
+            elif msg_words[0] == "!quit":
+                print "Exiting now."
+                self.die()
+            else:
+                print "Unrecognized command."
+                c.privmsg(user, "Unrecognized command.")
+        else:
+            print "Unauthorized user " + user + " issued private command " + msg
+
--- jadenbot.py
+++ jadenbot.py
@@ -11,61 +11,68 @@ from random import choice
 import jaden_irc
 
 default_config = { "api-key": "", "parts-of-speech": 
-	[ "noun", "adjective", "verb", "adverb", "interjection", "pronoun",
-	"preposition", "abbreviation", "affix", "article", "auxiliary-verb",
-	"conjunction", "definite-article", "family-name", "given-name",
-	"idiom", "imperative", "noun-plural", "noun-posessive",
-	"past-participle", "phrasal-prefix", "proper-noun",
-	"proper-noun-plural", "proper-noun-posessive", "suffix",
-	"verb-intransitive", "verb-transitive" ],
-	"base-url": "http://api.wordnik.com/v4",
-	"min-length": 5, "max-length": 9,
-	"min-corpus-count": 200, "max-corpus-count": -1,
-	"sentences":
-	[ "How Can [noun-plural] Be [adjective] If Our [noun-plural] Aren't [adjective] ?" ]
+    [ "noun", "adjective", "verb", "adverb", "interjection", "pronoun",
+    "preposition", "abbreviation", "affix", "article", "auxiliary-verb",
+    "conjunction", "definite-article", "family-name", "given-name",
+    "idiom", "imperative", "noun-plural", "noun-posessive",
+    "past-participle", "phrasal-prefix", "proper-noun",
+    "proper-noun-plural", "proper-noun-posessive", "suffix",
+    "verb-intransitive", "verb-transitive" ],
+    "base-url": "http://api.wordnik.com/v4",
+    "min-length": 5, "max-length": 9,
+    "min-corpus-count": 200, "max-corpus-count": -1,
+    "sentences":
+    [ "How Can [noun-plural] Be [adjective] If Our [noun-plural] Aren't [adjective] ?" ],
+        "server": "", "port": 6667, "channel": "", "nick": "JadenBot", 
+        "command-string": "!jaden", "bot-ops": []
 }
 
 def prompt_for_api_key():
-	print "You must add your own API key from http://developer.wordnik.com" \
-	      "under the field 'api-key' to use this program."
-	exit(0)
+    print "You must add your own API key from http://developer.wordnik.com" \
+          "under the field 'api-key' to use this program."
+    exit(1)
+
+def prompt_for_irc_connection(field):
+    print "Required field '" + field + "' not present in config.json."
+
+    exit(2)
 
 def initialize_config():
-	with open("config.json", "w") as config_file:
-		json.dump(default_config, config_file, indent=2)
+    with open("config.json", "w") as config_file:
+        json.dump(default_config, config_file, indent=2)
 
 def load_config():
-	if os.access("config.json", os.F_OK):
-		with open("config.json", "r") as config_file:
-			config = json.load(config_file)
-	else:
-		print "No config file found.  Generating default file in config.json"
-		initialize_config()
-		prompt_for_api_key()
-	
-	return config
+    if os.access("config.json", os.F_OK):
+        with open("config.json", "r") as config_file:
+            config = json.load(config_file)
+    else:
+        print "No config file found.  Generating default file in config.json"
+        initialize_config()
+        prompt_for_api_key()
+    
+    return config
 
 def init_api():
-	if config["api-key"] == "":
-		prompt_for_api_key()
-	
-	return swagger.ApiClient(config["api-key"], config["base-url"])
+    if config["api-key"] == "":
+        prompt_for_api_key()
+    
+    return swagger.ApiClient(config["api-key"], config["base-url"])
 
 def get_word(partOfSpeech):
-	try:
-		response = wordsApi.getRandomWord(includePartOfSpeech=partOfSpeech, \
-			minLength=config["min-length"], \
-			maxLength=config["max-length"], \
-			minCorpusCount=config["min-corpus-count"], \
-			maxCorpusCount=config["max-corpus-count"])
+    try:
+        response = wordsApi.getRandomWord(includePartOfSpeech=partOfSpeech, \
+            minLength=config["min-length"], \
+            maxLength=config["max-length"], \
+            minCorpusCount=config["min-corpus-count"], \
+            maxCorpusCount=config["max-corpus-count"])
 
-		if response is not None:
-			return response.word
-	except HTTPError:
-		print "Error finding word of type " + partOfSpeech
-		traceback.print_exc(file=sys.stderr)
+        if response is not None:
+            return response.word
+    except HTTPError:
+        print "Error finding word of type " + partOfSpeech
+        traceback.print_exc(file=sys.stderr)
 
-	return None
+    return None
 
 config = load_config()
 parts_of_speech = config["parts-of-speech"]
@@ -73,34 +80,33 @@ api = init_api()
 wordsApi = WordsApi.WordsApi(api)
 
 def generate_sentence():
-	words = choice(config["sentences"]).split(' ')
-	sentence = ""
+    words = choice(config["sentences"]).split(' ')
+    sentence = ""
 
-	for source_word in words:
-		source_word = re.sub("\[|\]", "", source_word)
-		if source_word.lower() in config["parts-of-speech"]:
-			word = get_word(source_word)
-			word = word.capitalize()
-		else:
-			word = source_word
-		
-		if word in "?!.":
-			sentence = sentence.rstrip()
-			sentence += word
-		else:
-			sentence += word + " "
-	
-	return sentence
+    for source_word in words:
+        source_word = re.sub("\[|\]", "", source_word)
+        if source_word.lower() in config["parts-of-speech"]:
+            word = get_word(source_word)
+            word = word.capitalize()
+        else:
+            word = source_word
+        
+        if word in "?!.":
+            sentence = sentence.rstrip()
+            sentence += word
+        else:
+            sentence += word + " "
+    
+    return sentence
 
 def main():
-	load_config()
+    for field in ["channel", "nick", "server"]:
+        if field not in config or config[field] == "":
+            prompt_for_irc_connection(field)
 
-	global parts_of_speech
-	parts_of_speech = config["parts-of-speech"]
-
-	irc_bot = jaden_irc.JadenBot(config["channel"], config["nick"], config["server"], config)
-	irc_bot.start()
+    irc_bot = jaden_irc.JadenBot(config["channel"], config["nick"], config["server"], config)
+    irc_bot.start()
 
 
 if __name__ == "__main__":
-	main()
+    main()
